9beedce9091e2499d52b373431dc8420dd0688ef
Mahatab14327 <mahabubkhosru543@gmail.com>
2025-09-17 17:34:21 +0600

    Add files via upload

---
+++ Toss Simulator/Toss Simulator.py
@@ -0,0 +1,9 @@
+import random
+
+print("Welcome to Toss Simulator")
+
+toss = random.choices(["Heads", "tails"])
+
+print("Result:", toss)
+
+print("Thanks for using Toss Simulator Take love from Mahatab :3")
