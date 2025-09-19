5aaaf4dfe7a8a76f956cba3f091bd1d07a4aa0d7
Mahatab14327 <mahabubkhosru543@gmail.com>
2025-09-19 19:16:40 +0600

    Add files via upload

---
+++ Automatic Joke Teller/Automatic Joke Teller.py
@@ -0,0 +1,21 @@
+print("Welcome to the Automatic Joke Teller!")
+
+import random
+
+jokes = jokes = [
+    "Why did the bicycle fall over? Because it was two-tired!",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "Why don't programmers like nature? Too many bugs.",
+    "What do you call fake spaghetti? An impasta!",
+    "Why did the math book look sad? Because it had too many problems!"
+]
+
+random.shuffle(jokes)
+for joke in jokes:
+
+ print("\nHere's a joke for you:")
+ print(joke)
+
+print("\nAll jokes have been told!")
+
+print("Thanks for Using Automatic Joke Teller Take love from Mahatab :3")
