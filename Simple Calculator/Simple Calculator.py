c65b7fdf3bbc8d36c10580d876fa2df5b2a58272
Mahatab14327 <mahabubkhosru543@gmail.com>
2025-09-27 18:24:08 +0600

    Add files via upload

---
+++ Simple Calculator/Simple Calculator.py
@@ -0,0 +1,9 @@
+print("Welcome to Simple calculator")
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("The sum is:", a+b)
+print("difference is:", a-b)
+print("pruduct is:",a*b)
+print("Division is:",a/b)
