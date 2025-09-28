d36813db895ab1f4f872985ba4a85f212340ca7a
Mahatab14327 <mahabubkhosru543@gmail.com>
2025-09-28 20:55:16 +0600

    Add files via upload

---
+++ Currency Converter(USD to BDT)/Currency Converter(USD to BDT).py
@@ -0,0 +1,7 @@
+print("Welcome To Currency Converter(USD to BDT)")
+
+usd = float(input("Enter Amount In USD$:"))
+BDT = usd * 108.61
+print("Your Amount In BDT IS:",BDT,)
+
+print("Thanks For Using Currency Converter Take Love From Mahatab :3")
