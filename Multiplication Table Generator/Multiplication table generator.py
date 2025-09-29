4ec184f744c81ad1d53b782f7c0109a4f84a7923
Mahatab14327 <mahabubkhosru543@gmail.com>
2025-09-29 20:59:55 +0600

    Add files via upload

---
+++ Multiplication Table Generator/Multiplication table generator.py
@@ -0,0 +1,12 @@
+print("Welcome to Multiplication Table Generator")
+
+num = int(input("Enter The Number:"))
+for i in range(1,11):
+ print(num, "x", i, "=", num * i)
+
+
+print("Thanks or using the Multiplication table generator take love from Mahatab :3")
+
+ 
+
+ 
