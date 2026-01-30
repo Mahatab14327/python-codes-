7081af29a79bd09f38faa0c0eeaac1c6a8c0e1dd
MAHATAB_14327 <mahabubkhosru543@gmail.com>
2026-01-30 20:09:10 +0600

    Add files via upload

---
+++ Vowel or Consonent checker/Vowel or Consonent checker.py
@@ -0,0 +1,13 @@
+print("Welcome to vowel or Consonent checker")
+
+char = input("Enter a letter:")
+
+vowels = ['a','e','i','o','u','A','E','I','O','U']
+
+if char in vowels:
+    print(char, "is a Vowel")
+else:
+    print(char, "is a Consonent")
+
+print("Thank you for using Vowel or Consonent checker take love from Mahatab :3")
+
