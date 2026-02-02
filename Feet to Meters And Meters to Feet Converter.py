3804911b138251ce943bd532f9b1a987e9ac537f
MAHATAB_14327 <mahabubkhosru543@gmail.com>
2026-02-02 19:18:58 +0600

    Delete Feet to Meters And Meters to Feet Converter.py

--- Feet to Meters And Meters to Feet Converter.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to Feet to Meters And Meters to Feet Converter!")
-
-choice = input("Type '1' to convert Feet to Meters or '2' to convert Meters to Feet: ")
-if choice == "1":
- feet = float(input("Enter the value in Feet:"))
- print("Meters:",round(feet/3.28, 3))
-else:
- meters = float(input("Enter the value in Meters:"))
- print("Feet:",round(meters*3.28, 3))
